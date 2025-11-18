241879742b72ac73d64517762a193bc7d152376a
HELLISH666-warframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2025-11-18 17:40:07 +1100

    Philly_mice_wip

--- data/stages/tweak4/park.hx
+++ data/stages/tweak4/park.hx
@@ -1,57 +1,21 @@
 var crowd:FlxSprite;
 
-defaultCamZoom = 0.55;
-
+//I_no_like_how_stage_xmls_handles_anims_ok?
 function create() {
-    var sky:FlxSprite = new FlxSprite(-600, -300);
-    sky.loadGraphic(Paths.image("stages/minus/sky"));
-    sky.antialiasing = true;
-    sky.scrollFactor.set(0.5, 0.5);
-    insert(1, sky);
-	add(sky);
-
-    var back:FlxSprite = new FlxSprite(-700, -100);
-    back.loadGraphic(Paths.image("stages/minus/back"));
-    back.antialiasing = true;
-    back.scrollFactor.set(0.8, 0.8);
-    insert(2, back);
-	add(back);
-
     crowd = new FlxSprite(-650, 480);
-    crowd.frames = Paths.getSparrowAtlas("stages/minus/crowd");
+    crowd.frames = Paths.getSparrowAtlas("stages/tweak4//minus/crowd");
     crowd.antialiasing = true;
     crowd.animation.addByPrefix('idle', 'crowd', 24, false);
     crowd.animation.play("idle");
     crowd.scrollFactor.set(0.9, 0.9);
     insert(3, crowd);
-	add(crowd); 
-
-    var front:FlxSprite = new FlxSprite(-900, 100);
-    front.loadGraphic(Paths.image("stages/minus/front"));
-    front.antialiasing = true;
-    front.scrollFactor.set(1, 1);
-    insert(4, front);
-	add(front);
-
-    }
-
-function beatHit()
-{
-    if(curBeat % 2 == 0){
-        crowd.animation.play('idle', true);
-        trace('POOPP)PPP');
-    }
-} 
-
-function onSongStart() {
-    crowd.animation.play('idle', true);
+    trace(boyfriend.x,boyfriend.y);
 }
 
-function onCountdownTick(swagCounter){
-    if(swagCounter % 2 == 0) crowd.animation.play('idle', true);
-}
+function beatHit() if(curBeat % 2 == 0) crowd.animation.play('idle', true);
 
-function onGameOverStart() 
-{
-    setGameOverVideo('minus');
-}
+function onSongStart() crowd.animation.play('idle', true);
+
+function onCountdownTick(swagCounter) if(swagCounter % 2 == 0) crowd.animation.play('idle', true);
+
+function onGameOverStart() setGameOverVideo('minus');
